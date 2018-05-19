4efefcdaade475101d96d9cee2701f6531ef7786
Pedro Bernardes <phec06@gmail.com>
2018-05-19 12:24:57 -0300

    Got ./bin/webpack-dev-server running :tada:

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -2,10 +2,12 @@ const { environment } = require('@rails/webpacker')
 const coffee =  require('./loaders/coffee')
 const vue =  require('./loaders/vue')
 const pug =  require('./loaders/pug')
-const sass =  require('./loaders/sass')
 
 environment.loaders.append('coffee', coffee)
 environment.loaders.append('vue', vue)
 environment.loaders.append('pug', pug)
-environment.loaders.append('sass', sass)
 module.exports = environment
+
+environment.loaders.get('sass').use.splice(-1, 0, {
+  loader: 'resolve-url-loader'
+});
